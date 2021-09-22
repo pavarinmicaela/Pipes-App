3181de4dee10551302d96c058c1a55fc6d3451d4
Micaela Pavarin <micaelapavarin@gmail.com>
2021-09-22 11:45:36 -0300

    Menu and routes

---
+++ src/app/app-router.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BasicsComponent } from './sales/pages/basics/basics.component';
+import { NoCommonsComponent } from './sales/pages/no-commons/no-commons.component';
+import { NumbersComponent } from './sales/pages/numbers/numbers.component';
+import { OrderComponent } from './sales/pages/order/order.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BasicsComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'no-commons',
+    component: NoCommonsComponent
+  },
+  {
+    path: 'numbers',
+    component: NumbersComponent
+  },
+  {
+    path: 'order',
+    component: OrderComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRouterModule { }
